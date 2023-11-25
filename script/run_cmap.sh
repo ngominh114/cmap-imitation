601ae05620c34c18862621d043070ee07ddc5d1f
Ngo Nhat Minh <ngominh114@gmail.com>
2023-11-25 17:14:46 +0200

    New script for running cmap with bash

---
+++ script/run_cmap.sh
@@ -0,0 +1,7 @@
+query_name=$1
+reference_data_file=$2
+gene_info_file=$3
+up_gene_file=$4
+down_gene_file=$5
+
+python ../src/cmap.py $query_name $reference_data_file $gene_info_file $up_gene_file $down_gene_file
