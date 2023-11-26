019ae6b3920be50b0661c86d7e5420ffa5b907b7
Ngo Nhat Minh <ngominh114@gmail.com>
2023-11-26 10:38:08 +0200

    Change node config

--- script/run_cmap.sh
+++ script/run_cmap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=cmap_run
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=128GB
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=256GB
 #SBATCH --time=01:00:00
 
 
